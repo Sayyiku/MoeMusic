04bf6a2133e9847023ec712be207992253cf4282
“Sayyiku” <sayyikue@gmail.com>
2022-03-16 00:55:24 +0800

    Init

---
+++ src/main/java/com/sayyiku/moemusic/MoeMusicApplication.java
@@ -0,0 +1,13 @@
+package com.sayyiku.moemusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoeMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoeMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sayyiku/moemusic/MoeMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sayyiku.moemusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoeMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
